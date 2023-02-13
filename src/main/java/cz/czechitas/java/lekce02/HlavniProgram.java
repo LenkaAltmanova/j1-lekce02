f1f928b79afd4bd32b869d8d60423eba83b21c53
Filip Jirsák <filip@jirsak.org>
2023-02-13 22:46:03 +0100

    Metoda start() je veřejná.

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -2,11 +2,110 @@ package cz.czechitas.java.lekce02;
 
 import cz.czechitas.java.lekce02.engine.Turtle;
 
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+
 public class HlavniProgram {
     private Turtle zofka;
-    private void start() {
+    public void start() {
         zofka = new Turtle();
 
+        for (int i = 0; i < 1; i++) {
+            zofka.turnRight(45);
+            zofka.move(71);
+            zofka.turnRight(90);
+            zofka.move(71);
+            zofka.turnRight(45);
+                    }
+
+        for(int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+                }
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.penDown();
+        zofka.turnLeft(90);
+
+        for (int i = 0; i < 1; i++) {
+            zofka.turnRight(45);
+            zofka.move(71);
+            zofka.turnRight(90);
+            zofka.move(71);
+            zofka.turnRight(45);
+        }
+
+        for(int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.penDown();
+        zofka.turnLeft(90);
+
+        for (int i = 0; i < 1; i++) {
+            zofka.turnRight(45);
+            zofka.move(71);
+            zofka.turnRight(90);
+            zofka.move(71);
+            zofka.turnRight(45);
+        }
+
+        for(int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+
+
+
+
+        /*zofka.turnLeft(90);
+        zofka.penUp();
+        zofka.move(200);
+        zofka.penDown();
+
+        zofka.turnRight(45);
+        zofka.move(71);
+        zofka.turnRight(90);
+        zofka.move(71);
+        zofka.turnRight(45);
+
+        for(int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+        zofka.turnLeft(45);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.turnRight(90);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.turnLeft(45);
+        zofka.move(100);
+        zofka.turnLeft(45);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.turnRight(90);
+        zofka.move(30);
+        */
+
+
+
+
+        /*zofka.turnLeft(90);
+        zofka.penUp();
+        zofka.move(200);
+        zofka.penDown();
+        zofka.turnRight(90);
+        for(int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }*/
         //TODO tady bude kód
     }
 
